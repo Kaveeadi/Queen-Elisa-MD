67dcef54e658e5da25e37da95b339471d3078f79
Kaveeadi <112240967+Kaveeadi@users.noreply.github.com>
2022-08-27 13:42:00 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -33,13 +33,13 @@ global.APIKeys = {
 
 //other
 global.owner = ['94706352627'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
-global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
-global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
-global.pengguna = 'i' // Your name ඔබේ නම 
-global.botnma = '💃 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
-global.ownernma = '𝓓𝓪𝓻𝓴 𝓜𝓪𝓴𝓮𝓻' //ownernama,ownername
+global.pemilik = ['94786959420'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
+global.premium = ['94786959420'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
+global.pengguna = 'Ƙίηɡ Ƙαѵίψα' // Your name ඔබේ නම 
+global.botnma = '💃Ƙίηɡ Ƙαѵίψα ɓσt💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
+global.ownernma = 'Ƙίηɡ Ƙαѵίψα' //ownernama,ownername
 global.packname = 'Queen Elisa Sticker' // Sticker package name 
-global.author = 'Dark Maker' // Sticker Autor name 
+global.author = 'Ƙίηɡ Ƙαѵίψα' // Sticker Autor name 
 global.sessionName = 'session'
 global.prefa = ['#','!','/',''] 
 global.sp = '🔵'
@@ -60,7 +60,7 @@ global.sendwelcome = `add`
 // Good bye එක ඔනි නම් remove කියලා දාන්න
 global.sendgoodbye = `remove`
 // Caption 
-global.cap = `© 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐁𝐲 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐌𝐃`
+global.cap = `© 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐁𝐲 💃Ƙίηɡ Ƙαѵίψα ɓσt💃`
 global.mess = {
     success: '✅ Done!',
     admin: 'you must be admin to use this cmd!',
